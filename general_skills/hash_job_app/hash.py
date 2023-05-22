72654d2ec79fff3fd04ec021b01a91fa1e0ba3d1
rcoder <richi_coder@gmx.de>
2023-05-22 16:49:40 +0200

    some more

---
+++ general_skills/hash_job_app/hash.py
@@ -0,0 +1,26 @@
+import hashlib
+import socket
+import time
+
+HOST = "saturn.picoctf.net"
+PORT = 49697
+
+prompt_msg = "Please md5 hash the text between quotes, excluding the quotes:"
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+
+    s.connect((HOST, PORT))
+    for _ in range(3):
+        prompt = s.recv(4096).decode()
+        # print(prompt)
+        target = [t.split("\'")[-2] for t in prompt.split("\n") if prompt_msg in t][0]
+        print(f"received target: {target}")
+        hash = hashlib.md5(target.encode()).hexdigest() +"\n"
+        print(f"sending md5 hash: {hash}")
+        #print(hash)
+        s.sendall(hash.encode())
+        data = s.recv(len(hash)) 
+        time.sleep(.5)
+    data = s.recv(1024) 
+
+    print("Your flag is: ", data.decode().split("\n")[-2])
