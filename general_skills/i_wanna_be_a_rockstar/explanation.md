396074180fa2d2d2bf53cfb6291a296cca7f1880
rcoder <richi_coder@gmx.de>
2023-05-23 14:11:28 +0200

    added some more in general skills

---
+++ general_skills/i_wanna_be_a_rockstar/explanation.md
@@ -0,0 +1,12 @@
+install tool:
+
+```
+pip3 install rockstar-py
+```
+
+Run comand:
+```
+rockstar-py -i lyrics.txt -o lyrics.py
+```
+
+flag picoCTF{BONJOVI}
---
+++ general_skills/i_wanna_be_a_rockstar/lyrics.py
@@ -0,0 +1,28 @@
+Rocknroll = True
+Silence = False
+a_guitar = 10
+Tommy = 44
+Music = 170
+the_music = input()
+if the_music == a_guitar:
+    print("Keep on rocking!")
+    the_rhythm = input()
+    if the_rhythm - Music == False:
+        Tommy = 66 #B
+        print(Tommy!)
+        Music = 79 #O
+        Jamming = 78 #N
+        print(Music!)
+        print(Jamming!)
+        Tommy = 74 #J
+        print(Tommy!)
+        They are dazzled audiences
+        print(it!)
+        Rock = 86 #V
+        print(it!)
+        Tommy = 73 #I
+        print(it!)
+        break
+        print("Bring on the rock!")
+        Else print("That ain't it, Chief")
+        break
